a4b294e4cecc1799488d07fa70c781f24cc22db1
Vladimir Yakovlev <nagos@inbox.ru>
2023-03-06 11:08:04 +0300

    Parsing of  theremaining messages

--- src/protocol/ptpv2.rs
+++ src/protocol/ptpv2.rs
@@ -1,5 +1,11 @@
 use deku::prelude::*;
 
+const MGS_SYNC: u8 = 0x00;
+const MGS_DELAY_REQ: u8 = 0x01;
+const MGS_FOLLOW_UP: u8 = 0x08;
+const MGS_DELAY_RESP: u8 = 0x09;
+const MGS_ANNOUNCE: u8 = 0x0b;
+
 #[derive(Debug, PartialEq, DekuRead, DekuWrite)]
 #[deku(endian = "big")]
 struct PtpHeaderProtocol {
@@ -44,6 +50,13 @@ struct PtpAnnouceProtocol {
     timesource: u8,
 }
 
+#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
+#[deku(endian = "big")]
+struct PtpDelayResp {
+    requestingsourceportidentity: u64,
+    requestingsourceportid: u16,
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -55,8 +68,49 @@ mod tests {
         let (rest, val) = PtpHeaderProtocol::from_bytes((&data, 0)).unwrap();
         assert_eq!(rest.0.len(), 20);
         assert_eq!(val.clockidentity, 0x485b39fffe11a8ab);
+        assert_eq!(val.messagetype, MGS_ANNOUNCE);
 
         let (_rest, val) = PtpAnnouceProtocol::from_bytes(rest).unwrap();
         assert_eq!(val.priority1, 127);
+    }
+
+    #[test]
+    fn delay_req_msg() {
+        let data = fs::read("src/protocol/test_data/msg_delay_req.bin").unwrap();
+        let (rest, val) = PtpHeaderProtocol::from_bytes((&data, 0)).unwrap();
+        assert_eq!(rest.0.len(), 0);
+        assert_eq!(val.clockidentity, 0x485B39FFFE520449);
+        assert_eq!(val.messagetype, MGS_DELAY_REQ);
+    }
+
+    #[test]
+    fn delay_resp_msg() {
+        let data = fs::read("src/protocol/test_data/msg_delay_resp.bin").unwrap();
+        let (rest, val) = PtpHeaderProtocol::from_bytes((&data, 0)).unwrap();
+        assert_eq!(rest.0.len(), 10);
+        assert_eq!(val.clockidentity, 0x485b39fffe11a8ab);
+        assert_eq!(val.messagetype, MGS_DELAY_RESP);
+
+        let (_rest, val) = PtpDelayResp::from_bytes(rest).unwrap();
+        assert_eq!(val.requestingsourceportid, 1);
+    }
+
+    #[test]
+    fn follow_up_msg() {
+        let data = fs::read("src/protocol/test_data/msg_follow_up.bin").unwrap();
+        let (rest, val) = PtpHeaderProtocol::from_bytes((&data, 0)).unwrap();
+        assert_eq!(rest.0.len(), 0);
+        assert_eq!(val.clockidentity, 0x485b39fffe11a8ab);
+        assert_eq!(val.messagetype, MGS_FOLLOW_UP);
+        assert_eq!(val.origintimestamp_nanoseconds, 521233408);
+    }
+
+    #[test]
+    fn sync_msg() {
+        let data = fs::read("src/protocol/test_data/msg_sync.bin").unwrap();
+        let (rest, val) = PtpHeaderProtocol::from_bytes((&data, 0)).unwrap();
+        assert_eq!(rest.0.len(), 0);
+        assert_eq!(val.clockidentity, 0x485b39fffe11a8ab);
+        assert_eq!(val.messagetype, MGS_SYNC);
     }
 }
